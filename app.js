04e9aceb8e1ea7358ef0b04958c52276e44fa073
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-12-25 16:39:03 +0700

    CRUD student

--- app.js
+++ app.js
@@ -2,8 +2,46 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Chú ý: chuyển req, res về json để tiện thao tác
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// lấy danh sách học sinh (url <=> http://localhost:3000/students)
+app.get("/students", (req, res) => {
+  res.send("lấy danh sách học sinh");
+});
+
+// lấy thông tin chi tiết của học sinh  (url <=> http://localhost:3000/students/1)
+app.get("/students/:id", (req, res) => {
+  const params = req.params;
+  console.log("params", params);
+  const { id } = params;
+  res.send("lấy thông tin chi tiết cuar học sinh có id là : " + id);
+});
+
+// thêm học sinh  (url <=> http://localhost:3000/students)
+app.post("/students", (req, res) => {
+  const student = req.body;
+  console.log(student);
+  res.send("thêm học sinh");
+});
+
+// cập nhật học sinh  (url <=> http://localhost:3000/students/1)
+app.put("/students/:id", (req, res) => {
+  const { id } = req.params;
+  const student = req.body;
+  console.log("id :", id);
+  console.log("student : ", student);
+  res.send("cập nhật học sinh");
+});
+
+// xoá học sinh  (url <=> http://localhost:3000/students/1)
+app.delete("/students/:id", (req, res) => {
+  const { id } = req.params;
+  res.send("xoá học sinh có id là : " + id);
 });
 
 app.listen(port, () => {
